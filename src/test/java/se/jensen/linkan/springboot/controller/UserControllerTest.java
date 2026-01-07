2f0e7a00592e2c44e69a247d45ace5199db688de
Linkan <thelinussandelin@gmail.com>
2026-01-07 10:56:10 +0100

    Social Web Application v2

--- src/test/java/se/jensen/linkan/springboot/controller/UserControllerTest.java
+++ src/test/java/se/jensen/linkan/springboot/controller/UserControllerTest.java
@@ -1,66 +1,63 @@
-package se.jensen.linkan.springboot.controller;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import se.jensen.linkan.springboot.dto.UserResponseDTO;
-import se.jensen.linkan.springboot.model.User;
-import se.jensen.linkan.springboot.repository.UserRepository;
-import tools.jackson.core.type.TypeReference;
-import tools.jackson.databind.ObjectMapper;
-
-import java.util.List;
-
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest
-@Disabled
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
-public class UserControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @BeforeEach
-    public void setUp() {
-        userRepository.deleteAll();
-        User user = new User();
-        user.setRole("ADMIN");
-        user.setPassword(passwordEncoder.encode("password"));
-        user.setEmail("admin@example.com");
-        user.setDisplayName("admin display");
-        user.setBio("admin bio");
-        user.setUsername("admin");
-        userRepository.save(user);
-    }
-
-    @Test
-    @Disabled
-    void shouldGetAllUsers() throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/users")
-                        .with(httpBasic("admin", "password")))
-                .andExpect(status().isOk()).andReturn();
-        String response = result.getResponse().getContentAsString();
-        List<UserResponseDTO> users = objectMapper.readValue(
-                response, new TypeReference<List<UserResponseDTO>>() {
-                });
-        Assertions.assertEquals(1, users.size());
-
-    }
-}
+//package se.jensen.linkan.springboot.controller;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MvcResult;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import se.jensen.linkan.springboot.dto.UserResponseDTO;
+//import se.jensen.linkan.springboot.model.User;
+//import se.jensen.linkan.springboot.repository.UserRepository;
+//import tools.jackson.core.type.TypeReference;
+//import tools.jackson.databind.ObjectMapper;
+//
+//import java.util.List;
+//
+//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//@ActiveProfiles("test")
+//public class UserControllerTest {
+//    @Autowired
+//    private MockMvc mockMvc;
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//    @Autowired
+//    private UserRepository userRepository;
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    @BeforeEach
+//    public void setUp() {
+//        userRepository.deleteAll();
+//        User user = new User();
+//        user.setRole("ADMIN");
+//        user.setPassword(passwordEncoder.encode("password"));
+//        user.setEmail("admin@example.com");
+//        user.setDisplayName("admin display");
+//        user.setBio("admin bio");
+//        user.setUsername("admin");
+//        userRepository.save(user);
+//    }
+//
+//    @Test
+//    void shouldGetAllUsers() throws Exception {
+//        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/users")
+//                        .with(httpBasic("admin", "password")))
+//                .andExpect(status().isOk()).andReturn();
+//        String response = result.getResponse().getContentAsString();
+//        List<UserResponseDTO> users = objectMapper.readValue(
+//                response, new TypeReference<List<UserResponseDTO>>() {
+//                });
+//        Assertions.assertEquals(1, users.size());
+//
+//    }
+//}
